6a8c47ad23170be2529a3f198c674760b6bab46c
Iliya Chilev <ichilev@gmail.com>
2024-12-02 23:47:44 +0200

    migrations

---
+++ database/migrations/2024_12_02_214537_create_comment_reactions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment_reactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('comments_id')->constrained('comments')->onDelete('cascade');
+            $table->foreignId('reactions_types_id')->constrained('reactions_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment_reactions');
+    }
+};
