d7065f0f352736b579182338404555438e1e14ec
Iliya Chilev <ichilev@gmail.com>
2024-12-22 10:19:58 +0200

    filesystems adjustments

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,35 +38,20 @@ return [
         ],
         'production' => [
             'driver' => 'local',
-            'root' => '/mnt/volume_fra1_01/storage',
-            'serve' => true,
+            'root' => '/mnt/volume_fra1_01',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'private',
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
+            'root' => '/mnt/volume_fra1_01',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'private',
             'throw' => false,
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-        ],
-        'gcs' => [
-            'driver' => 'gcs',
-            'bucket' => env('GCS_BUCKET', 'plantaskr_bucket'), // Add this line
-            'keyFile' => json_decode(getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON'), true),
-        ],
 
     ],
 
